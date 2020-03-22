00efb23d4a7ecc61df40c0e147f4647de657ee84
Mustafa Raad <ofydesigner.2008@gmail.com>
2020-03-22 03:41:33 +0300

    About-us Completed

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"]
 })
 export class AboutComponent implements OnInit {
+  mr: boolean;
+  ma: boolean;
+  mm: boolean;
+  mrj: boolean;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
