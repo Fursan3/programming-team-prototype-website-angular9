5e73291f66be213ed98fec18cccf96a33c08effe
Mustafa Raad <ofydesigner.2008@gmail.com>
2020-03-23 02:14:47 +0300

    Remove slider's comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,23 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 // --------------------------------Router-------------------------------------
 import { AppRoutingModule } from "./app-routing.module";
-// --------------------------------HTTP client-------------------------------------\
+// --------------------------------HTTP client-------------------------------------
 import { HttpClientModule } from "@angular/common/http";
 // import { AgmCoreModule } from "@agm/core";
+// --------------------------------rxjs-------------------------------------
+import { from } from 'rxjs';
 // --------------------------------Components-------------------------------------
 import { AppComponent } from "./app.component";
 import { NavbarComponent } from "./navbar/navbar.component";
-import { SlidersComponent } from "./sliders/sliders.component";
 import { HireComponent } from "./hire/hire.component";
 import { ContactComponent } from "./contact/contact.component";
 import { AboutComponent } from "./about/about.component";
-import { from } from 'rxjs';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SlidersComponent,
     HireComponent,
     ContactComponent,
     AboutComponent,
