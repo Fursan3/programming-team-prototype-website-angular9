fe2274f1bc8f43078ad6ca809992b148af41d1b3
mohammedaltaei <62484649+mohammedaltaei@users.noreply.github.com>
2020-03-23 18:11:55 +0300

    contact final

---
+++ contact.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+  Name:string;
+
+  constructor() { }
+  latitude = 33.312805;
+  longitude = 44.361488;
+  ngOnInit(): void {
+
+  }
+
+
+
+}
